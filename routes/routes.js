6f80d9cc47f9bf5eb34c96eb766b4774efb2f0db
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-03-21 04:55:45 +0000

    heroku test

---
+++ routes/routes.js
@@ -0,0 +1,24 @@
+//const path = process.cwd();
+const crypto = require('crypto');
+//const bodyParser = require('body-parser');
+const argon2i = require('argon2-ffi').argon2i;
+const users = require("../model/users.js");
+
+
+const MIN_PASSWORD_LENGTH = 8;
+const MAX_PASSWORD_LENGTH = 160;
+
+
+
+
+
+
+module.exports = function(app){
+  app.route("/")
+    .get(function(req, res){
+      console.log("Ok")
+      res.send("OK");
+    });
+  
+
+};
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const routes = require("./routes/routes.js");
 
 const app = express();
 
+app.use('/control', express.static(process.cwd() + '/control'));
+app.use('/view', express.static(process.cwd() + '/view'));
+app.use('/common', express.static(process.cwd() + '/common'));
 
+routes(app);
 
 //create server
 app.listen(process.env.PORT || 8080, function(){
