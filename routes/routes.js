5f9430e5ad861894a96b08112d00b88957cfcdd2
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-03-22 10:11:19 +0000

    tidy addAsset api

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 const path = process.cwd();
 const argon2i = require('argon2-ffi').argon2i;
 const UserHandler = require("../control/userControl.server.js");
+const AssetHandler = require("../control/assetControl.server.js");
 
 
 module.exports = function(app, passport){
   const userHandler = new UserHandler();
+  const assetHander = new AssetHandler();
   app.route("/")
     .get(function(req, res){
       console.log("Ok")
@@ -40,10 +42,10 @@ module.exports = function(app, passport){
     
   /*********API********/
   app.route('/api/addasset')
-    .get()
-    .post();
+    .get(assetHander.addAsset)
+    .post(assetHander.addAsset);
     
-  app.route('/api/assignasset')
+  /*app.route('/api/assignasset')
     .get()
     .post();
     
@@ -57,7 +59,7 @@ module.exports = function(app, passport){
     
   app.route('/api/asset')
     .get()
-    .post();
+    .post();*/
     
     
     
