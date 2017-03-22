a46e60df3e61ea35aade2a3f161cf0dd1c23b154
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-03-22 17:16:50 +0000

    add cancel button for addAdmin

--- control/userControl.super.js
+++ control/userControl.super.js
@@ -3,9 +3,12 @@
   const adminBox = document.getElementById('addAdminBox');
   const adminDisplayButton = document.getElementById('adminDisplayButton');
   const addAdminButton = document.getElementById('addAdminButton');
+  const cancelAdminButton = document.getElementById("cancelAdminButton");
   
   adminDisplayButton.addEventListener('click', function(){adminBox.style.display = 'block'}, false);
   addAdminButton.addEventListener('click', addAdmin, false);
+  cancelAdminButton.addEventListener('click', function(){adminBox.style.display = 'none'}, false);
+  
   
   //Add new admin
   function addAdmin(){
