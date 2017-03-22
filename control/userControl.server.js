3c1503fc6d7002d146aa6e80ec63251886174761
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-03-22 11:55:04 +0000

    add addAdmin control

--- control/userControl.server.js
+++ control/userControl.server.js
@@ -4,6 +4,20 @@ const User = require(path + '/model/users.js');
 
 
 function UserHandler() {
+  this.addadmin = function(req, res){
+    const username = req.query.username;
+    User.findOneAndUpdate({$and: [{username : username}, {department : 'Operations and Facilities'}, {accounttype : 'ordinary'}]},
+    { $set: { accounttype : 'admin'}},{new : true},
+      function(err, result){
+        if (err) throw err;
+        if(result){
+          res.send('added as admin');
+        }else{
+          res.send('No user, or wrong department or user is already an admin');
+        }
+        
+      });
+  };
   
   this.superAdmin = function(req, res){
     res.sendFile(path + "/public/superadmin.html");
