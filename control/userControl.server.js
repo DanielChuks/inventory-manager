a6e07d12743aa588746e1692977c699f9c0f8a59
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-03-22 11:32:11 +0000

    remove signup control

--- control/userControl.server.js
+++ control/userControl.server.js
@@ -4,36 +4,6 @@ const User = require(path + '/model/users.js');
 
 
 function UserHandler() {
-  //log users in using the proper authentications
-  this.login = function(req, res){
-    const query = req.body;
-    const username = query.username;
-    const password = query.password;
-    console.log(req.body);
-    console.log(password, username)
-    User.findOne({username: username})
-      .exec(function(err, result){
-        if(err){
-          throw err;
-        }else{
-          console.log(result)
-          if(result.accounttype === 'superadmin'){
-            console.log('Super');
-            res.redirect('/:username/admin/super')
-            //redirect to superadmin
-          }else if(result.accounttype === 'admin'){
-            console.log('Admin');
-            res.redirect('/:username/admin');
-            //redirect to admin page
-          }else{
-            console.log('Ord')
-            res.redirect('/:username');
-            //redirect to normal user page
-          }
-          
-        }
-      });
-  };
   
   this.superAdmin = function(req, res){
     res.sendFile(path + "/public/superadmin.html");
